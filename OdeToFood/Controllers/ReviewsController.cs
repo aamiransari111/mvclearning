7104e42bca362f42fd2208f29fd4e5de329e30dd
aamiransari111 <aamiransari111@gmail.com>
2019-07-15 21:20:03 +0530

    crud operations

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -1,144 +1,63 @@
-﻿//using OdeToFood.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace OdeToFood.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
 
-//        public ActionResult BestReview()
-//        {
-//            var review = from r in _reviews
-//                         orderby r.Rating descending
-//                         select r;
-//            return PartialView("_Reviews",review.First());
-//        }
-//        //
-//        // GET: /Reviews/
-//        public ActionResult Index()
-//        {
-//            var  model=
-//                from r in _reviews
-//                orderby r.Country
-//                select r;
+        OdeToFoodDB _db = new OdeToFoodDB();
+        // GET: /Reviews/
+        public ActionResult Index([Bind(Prefix="id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+                return View(restaurant);
+            return HttpNotFound();
 
-//            return View(model);
-//        }
-
-//        //
-//        // GET: /Reviews/Details/5
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // GET: /Reviews/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Create
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        //
-//        // GET: /Reviews/Edit/5
-//        public ActionResult Edit(int id)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            return View(review);
-//        }
-
-//        //
-//        // POST: /Reviews/Edit/5
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            if(TryUpdateModel(review))
-//            {
-//                return RedirectToAction("Index");
-//            }
-//            return View(review);
-//        }
-
-//        //
-//        // GET: /Reviews/Delete/5
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Delete/5
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-//        static List<RestaurantReview> _reviews = new List<RestaurantReview>
-//        {
-//            new RestaurantReview
-//            {
-//                Id= 1,
-//                Name="Persian Darbar",
-//                City="Mumbai",
-//                Country="India",
-//                Rating=5
-//            },
-//            new RestaurantReview
-//            {
-//                Id= 2,
-//                Name="Shalimar",
-//                City="Mumbai",
-//                Country="India",
-//                Rating=5
-//            },
-//            new RestaurantReview
-//            {
-//                Id= 3,
-//                Name="Delhi Darbar",
-//                City="Mumbai",
-//                Country="India",
-//                Rating=5
-//            },
-//            new RestaurantReview
-//            {
-//                Id= 4,
-//                Name="5 Spice",
-//                City="Mumbai",
-//                Country="India",
-//                Rating=5
-//            }
-
-//        };
-//    }
-//}
+        }
+        [HttpGet]
+        public ActionResult Create(int restaurantId)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+            return View(review);
+        }
+        
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = _db.Reviews.Find(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(RestaurantReview review)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Entry(review).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+            return View(review);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
